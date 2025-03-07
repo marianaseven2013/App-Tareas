491f9b6561f7e947a56be99b06c08f8eb6526ea1
Genesis <gmluch@scl.edu.gt>
2025-03-07 17:25:57 -0600

    Se extraen datos de todo_list para la seccion de formulario

--- componentes/formulario/funcionesformulario.js
+++ componentes/formulario/funcionesformulario.js
@@ -1,14 +1,41 @@
-import { Cargartarea } from "../tarea/tarea.js";
+import { consultarTareasbackEnd } from "../tarea/tarea.js";
 
 function agregartarea(){
+   
     //En esta variable va a almacenar aquel elemento cuya clase se llama entradaformulaio, esto va a recuperar su contenido, ya sea texto etc.
    let tareaForm = document.querySelector(".fori").value; 
-   listado.unshift(tareaForm); //agregar contenido a la lista, por eso se llama a la lista, lo que le indica agregar ese contenido.
 
-   let prueba = document.querySelector("#secciontt");
-   prueba.innerHTML = '';//Limpia y agrega lo nuevo.
-   prueba.appendChild(Cargartarea());
 
+   fetch('http://localhost:3000/agregar', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      nombre_tarea: tareaForm,
+      estado: 'pendiente'
+    })
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Error al guardar la tarea');
     }
+    return response.json();
+  })
+  .then(data => {
+    console.log('Tarea agregada con éxito:', data);
+  })
+  .catch(error => {
+    console.error('Hubo un problema con la solicitud:', error);
+  });
+
+   
+/*    let prueba = document.querySelector("#secciontt");
+   prueba.innerHTML = '';//Limpia y agrega lo nuevo.
+   prueba.appendChild(consultarTareasbackEnd()); */
+
+   document.querySelector("#root").innerHTML = "";
+
+}
 
 export {agregartarea}
--- index.js
+++ index.js
@@ -2,16 +2,17 @@ import { header } from "./componentes/header/header.js";
 import { consultarTareasbackEnd } from "./componentes/tarea/tarea.js";
 /* import { rendeTareas } from "./componentes/tarea/tarea.js";
  */
+import { formulario } from "./componentes/formulario/formulario.js";
 let DOM = document.querySelector("#root");
 
 function cargarDOM(){
    DOM.appendChild(header());
 /*    /* let seccionTa = document.createElement('div'); */
   consultarTareasbackEnd();
-   /* seccionTa.id = 'secciontt';
+  /*  seccionTa.id = 'secciontt';
    seccionTa.appendChild((rendeTareas));
-   DOM.appendChild(seccionTa);
-   DOM.appendChild(formulario()); */
+   DOM.appendChild(seccionTa); */
+   DOM.appendChild(formulario());
 }
 
 
